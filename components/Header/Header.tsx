64979805dcaf7c332bb5f8c9e87943ba9036d078
Ramazan <ramazandjaparov@gmail.com>
2025-08-12 20:41:13 +0600

    .

---
+++ components/Header/Header.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import style from "./Header.module.css";
+import { headerData } from "./HeaderData";
+
+export const Header = () => {
+  return (
+    <div className={style.header}>
+      <div className={style.navbar}>
+        <div>
+          <Image src="/svg/logo.svg" width={113} height={37} alt="logo" />
+        </div>
+        <ul className={style.list}>
+          {headerData.map((item, index) => (
+            <li key={index} className={style.link}>
+              {item.link}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
---
+++ components/Header/HeaderData.tsx
@@ -0,0 +1,21 @@
+interface headerDataInterface {
+  link: string;
+}
+
+export const headerData = [
+  {
+    link: "Главная",
+  },
+  {
+    link: "О нас",
+  },
+  {
+    link: "Навыки",
+  },
+  {
+    link: "Проекты",
+  },
+  {
+    link: "Контакты",
+  },
+];
